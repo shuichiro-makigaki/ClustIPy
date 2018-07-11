8123ca8fe4ef8d1f31cf6c952103b9bdbd108b57
Shuichiro MAKIGAKI <shuichiro.makigaki@gmail.com>
2018-07-11 16:30:33 +0900

    Add field number option

--- cli.py
+++ cli.py
@@ -11,14 +11,15 @@ from clustipy import clustipy
 @click.option('--incsv', help='Input CSV', required=True, type=click.Path())
 @click.option('--nclusters', help='Number of clusters', required=True, type=int)
 @click.option('--nocsvheader', help='Do not ignore the first line of CSV', is_flag=True, default=False)
-def main(incsv, nclusters, nocsvheader):
+@click.option('--nfield', help='Column number', type=int, default=0)
+def main(incsv, nclusters, nocsvheader, nfield):
     networks = []
     with Path(incsv).open() as f:
         reader = csv.reader(f)
         if not nocsvheader:
             next(reader)
         for row in reader:
-            networks.append(netaddr.IPNetwork(row[0].split('/')[0]))
+            networks.append(netaddr.IPNetwork(row[nfield].split('/')[0]))
     result = clustipy.clustering(networks, nclusters)
     for r in result:
         print(r)
