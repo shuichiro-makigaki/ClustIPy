d089bb1c2864bd18d342dec28b887bd77a8ffcea
Shuichiro MAKIGAKI <shuichiro.makigaki@gmail.com>
2020-01-27 15:59:21 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ClustIPy',
-    version='0.1.1',
+    version='0.1.0',
     packages=find_packages(),
     include_package_data=True,
     url='https://github.com/shuichiro-makigaki/ClustIPy',
@@ -10,6 +10,8 @@ setup(
     author='Shuichiro MAKIGAKI',
     author_email='shuichiro.makigaki@gmail.com',
     description='Clustering IPs',
+    long_description=Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
     entry_points='''
         [console_scripts]
         clustipy=clustipy.cmd.clustipy:main
